c32b704078a86a4d66ccded010a00ab65ce9ada0
shiva-manu <shivamani6964@gmail.com>
2024-05-30 18:27:15 +0530

    MongoDB connected

--- backend/db/connectMongoDb.js
+++ backend/db/connectMongoDb.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
-
-const connectMongoDB=async ()=>{
+const connectMongoDB=async()=>{
    try{
-      const user=await mongoose.connect(process.env.MONGO_URI);
-      console.log(`MongoDB connected: ${user.connection.host}`);
+      const conn=await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB connected: ${conn.connection.host}`);
    }catch(error){
-      console.log(`Error connecting to MongoDB: ${error.message}`);
-      process.exit(1)
+      console.log(`Error connection o mongoDB:${error.message}`);
+      process.exit(1);
    }
 }
-
 export default connectMongoDB
