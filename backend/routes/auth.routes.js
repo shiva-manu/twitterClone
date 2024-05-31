63376902992c88e348c55183610262a06363e92c
shiva-manu <shivamani6964@gmail.com>
2024-05-31 11:18:17 +0530

    add getMe route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {signup,login,logout} from "../controllers/auth.controller.js"
+import {signup,login,logout, getme} from "../controllers/auth.controller.js";
+import { protectRoute } from '../middleware/protectRoute.js';
 const router=express.Router();
 
+router.get("/me",protectRoute,getme)
 router.post("/signup",signup)
 router.post("/login",login)
-
-router.post("logout",logout);
+router.post("/logout",logout);
 export default router;
