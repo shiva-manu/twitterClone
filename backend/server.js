932ccbce8de8ef7a86edc407f56e08e2eeb4abf3
shiva-manu <shivamani6964@gmail.com>
2024-06-01 23:55:44 +0530

    updateProfile add

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import dotenv from 'dotenv';
+import {v2 as cloudinary} from "cloudinary";
 import connectMongoDB from "./db/connectMongoDb.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
+cloudinary.config({
+   cloudName:process.env.CLOUDINARY_CLOUD_NAME,
+   apiKey:process.env.CLOUDINARY_API_KEY,
+   apiSecret:process.env.CLOUDINARY_API_SECRET,
+})
 const app=express();
 app.use(express.urlencoded({extended:true}));
 const port=process.env.PORT || 5000;
