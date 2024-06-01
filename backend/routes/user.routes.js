f3a0292d4fb77476c4bc53c58688d578aafb68b4
shiva-manu <shivamani6964@gmail.com>
2024-06-01 23:51:29 +0530

    user route add

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { getUserprofile,followUnfollowUser } from "../controllers/user.controller.js";
+import { getUserprofile,followUnfollowUser,getSuggestedUser,updateUserProfile} from "../controllers/user.controller.js";
 const router=express.Router();
 
 router.get("/profile/:userName",protectRoute,getUserprofile);
-// router.get("/suggested",protectRoute,getUserprofile);
+router.get("/suggested",protectRoute,getSuggestedUser);
 router.post("/follow/:id",protectRoute,followUnfollowUser);
-// router.post("/update",protectRoute,updateUserProfile);
+router.post("/update",protectRoute,updateUserProfile);
 export default router;
