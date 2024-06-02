e681d6d97a9a8421b02027234319f4d17c30a946
shiva-manu <shivamani6964@gmail.com>
2024-06-02 20:46:19 +0530

    some changes made

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import dotenv from 'dotenv';
 import {v2 as cloudinary} from "cloudinary";
 import connectMongoDB from "./db/connectMongoDb.js";
@@ -20,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes);
-
+app.use("/api/posts",postRoutes)
 app.get("/",(req,res)=>{
    res.send("Server is ready");
 });
